38f46bb50c172c07ed966526019d77e6704798f7
nab-sa <nab_s_a@protonmail.com>
2023-03-22 16:04:31 +0100

    add user schema

---
+++ src/models/user.ts
@@ -0,0 +1,11 @@
+import { mongoose, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema)
+
+export default User;
